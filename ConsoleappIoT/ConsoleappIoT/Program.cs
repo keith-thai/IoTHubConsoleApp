5b8c22a532247ebe2a4fce25cb2ffa942aa28f5d
Keith Thai <Keith_Thai@oxy.com>
2021-01-06 00:14:07 -0600

    4th Committed from local to GitHub to trigger ADO build

--- ConsoleappIoT/ConsoleappIoT/Program.cs
+++ ConsoleappIoT/ConsoleappIoT/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleappIoT
 {
     // KTT - 1/5/2021 - 1st Committed from local 
     // KTT - 1/5/2021 - 3rd Committed from local
+    // KTT - 1/5/2021 - 4th Committed from local to GitHub to trigger ADO build
     class Program
     {
         //You can get the DeviceConnectionString from your IoT Hub on the Azure Portal
